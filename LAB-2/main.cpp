19852afd7a7fc4e4dc11a25a2bbf427b5ba41719
tahaorcunkara26 <tahaorcunn132@gmail.com>
2026-03-11 11:58:00 +0300

    Initialize main.cpp with structure and undo function
    Added basic structure and function definitions in main.cpp.

--- LAB-2/main.cpp
+++ LAB-2/main.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+void undo();
+int main()
+{
+    typedef struct t
+    {
+        string ad;
+        *node name;
+    }
+    return 0;
+}
+void undo()
+{
+    //
+}
